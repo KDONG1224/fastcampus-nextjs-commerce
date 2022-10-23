97864cb7e837871d28ecf5902afdbbd60b2da880
강동재 <gangdongjae@gangdongjaes-MacBook-Air.local>
2022-10-23 21:21:18 +0900

    image carousel setting...

---
+++ src/pages/products.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+
+export const sampleImages = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1022/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1022/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/'
+  }
+];
+
+const Products = () => {
+  return <ImageGallery items={sampleImages} />;
+};
+
+export default Products;
---
+++ src/pages/productsV2.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Carousel from 'nuka-carousel';
+import { sampleImages } from './products';
+import { css } from '@emotion/react';
+
+const ProductsV2 = () => {
+  const [index, setIndex] = useState(0);
+  return (
+    <>
+      <Carousel
+        animation="fade"
+        autoplay
+        withoutControls
+        wrapAround
+        speed={10}
+        slideIndex={index}
+      >
+        {sampleImages.map((image) => (
+          <Image
+            key={image.original}
+            src={image.original}
+            alt={image.thumbnail}
+            width={1000}
+            height={600}
+            layout="responsive"
+          />
+        ))}
+      </Carousel>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        {sampleImages.map((item, idx) => (
+          <div key={idx} onClick={() => setIndex(idx)}>
+            <Image src={item.original} alt="image" width={100} height={60} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductsV2;
